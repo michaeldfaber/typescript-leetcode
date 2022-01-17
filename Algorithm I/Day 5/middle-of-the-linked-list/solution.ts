a4ffa901e8d51e9b3506b41ae8304e46d62bf141
Michael Faber <michaelf1444@gmail.com>
2022-01-16 18:59:54 -0600

    middle-of-the-linked-list alternative with Map

--- Algorithm I/Day 5/middle-of-the-linked-list/solution.ts
+++ Algorithm I/Day 5/middle-of-the-linked-list/solution.ts
@@ -15,3 +15,27 @@ function middleNode(head: ListNode | null): ListNode | null {
     
     return head;
 };
+
+// alternative solution with a Map
+// not great for space complexity
+function middleNode2(head: ListNode | null): ListNode | null {
+    if (head.next === null) {
+        return head;
+    }
+    
+    if (head.next.next === null) {
+        return head.next;
+    }
+    
+    let linkedlist = head;
+    let map: Map<number, ListNode> = new Map();
+    let length = 0;
+    
+    while (linkedlist.next !== null) {
+        map[length] = linkedlist;
+        linkedlist = linkedlist.next;
+        length++;
+    }
+    
+    return map[Math.ceil(length/2)];
+};
