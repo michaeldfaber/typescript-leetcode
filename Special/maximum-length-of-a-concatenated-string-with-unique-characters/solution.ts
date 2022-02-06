8397ee2ffbfaf4de5f8f1af6bfcf2f5e033caf1d
Michael Faber <michaelf1444@gmail.com>
2022-02-06 07:58:18 -0600

    maximum-length-of-a-concatenated-string-with-unique-characters comments

--- Special/maximum-length-of-a-concatenated-string-with-unique-characters/solution.ts
+++ Special/maximum-length-of-a-concatenated-string-with-unique-characters/solution.ts
@@ -1,11 +1,19 @@
 function maxLength(arr: string[]): number {
+    // keep track of max
     let max = 0;
+    
+    // keep track of valid concatentations
     let concats: string[] = [""];
     
+    // loop through array
     for (let i = 0; i < arr.length; i++) {
+        // loop through valid concatentations
         for (let j = 0; j < concats.length; j++) {
+            // check if concatentation of element in array and current concat is unique
+            // adds arr[i] because concat is initialized with ""
             if (isUnique(arr[i] + concats[j])) {
                 concats.push(arr[i] + concats[j]);
+                // check if max
                 if (max < (arr[i] + concats[j]).length) {
                     max = (arr[i] + concats[j]).length;
                 }
