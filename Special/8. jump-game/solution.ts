37bba1415673cc3f3423e4eb489f4af752fdc71a
Michael Faber <michaelf1444@gmail.com>
2022-02-06 12:23:16 -0600

    jump-game

---
+++ Special/8. jump-game/solution.ts
@@ -0,0 +1,7 @@
+function canJump(nums: number[]): boolean {
+    let jump = nums.length - 1;
+    for (let i = nums.length - 2; i >= 0; i--) {
+        if (i + nums[i] >= jump) jump = i;
+    }
+    return jump === 0;
+}
